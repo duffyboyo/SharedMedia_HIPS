fb80ded99e63765cf6a08f68dfcf17cf9bb5a688
Adam Dufton <mail@duffyboyo.com>
2021-07-22 21:22:31 +0100

    initial commit

---
+++ main.lua
@@ -0,0 +1,30 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+if (LSM == nil) then 
+	return 
+end
+
+--background:
+
+--border:
+
+--font:
+
+--sound:
+
+LSM:Register("sound", "HIPS: Healthy Glug", [[Interface\Addons\SharedMedia_HIPS\sound\healthyglug.mp3]])
+LSM:Register("sound", "HIPS: Awh", [[Interface\Addons\SharedMedia_HIPS\sound\awh.mp3]])
+LSM:Register("sound", "HIPS: Bomb", [[Interface\Addons\SharedMedia_HIPS\sound\bomb.mp3]])
+
+--statusbar:
+
+
+
+--end 
+
+-- LSM:Register("background", "my background's name", [[Interface\Addons\SharedMedia_MyMedia\background\mybackground.tga]])
+-- LSM:Register("border", "my border's name", [[Interface\Addons\SharedMedia_MyMedia files\myborder.tga]])
+-- LSM:Register("font", "my font's name", [[Interface\Addons\SharedMedia_MyMedia\font\myfont.ttf]])
+-- LSM:Register("font", "my friend's font", [[Interface\Addons\SharedMedia_MyMedia\font\friendsfont.ttf]])
+-- LSM:Register("sound", "my sound's name", [[Interface\Addons\SharedMedia_MyMedia\sound\mysound.mp3]])
+-- LSM:Register("statusbar", "my statusbar texture's name", [[Interface\Addons\SharedMedia_MyMedia\statusbar\mytexture.tga]])
