73a56e39c86903e462704c056e0358a2cf8b1b15
Adam Dufton <mail@duffyboyo.com>
2021-07-30 13:56:35 +0100

    Add 3 new sounds
    Three new sounds have been added:
    * GreatDad
    * Raid Warning
    * Zug Zug Kick

--- main.lua
+++ main.lua
@@ -17,6 +17,9 @@ print("HIPS Shared media Loaded")
 LSM:Register("sound", "HIPS: Healthy Glug", [[Interface\Addons\SharedMedia_HIPS\sound\healthyglug.mp3]])
 LSM:Register("sound", "HIPS: Awh", [[Interface\Addons\SharedMedia_HIPS\sound\awh.mp3]])
 LSM:Register("sound", "HIPS: Bomb", [[Interface\Addons\SharedMedia_HIPS\sound\bomb.mp3]])
+LSM:Register("sound", "HIPS: GreatDad", [[Interface\Addons\SharedMedia_HIPS\sound\GreatDad.mp3]])
+LSM:Register("sound", "HIPS: Raid Warning", [[Interface\Addons\SharedMedia_HIPS\sound\Raidwarning.mp3]])
+LSM:Register("sound", "HIPS: ZugZug Kick", [[Interface\Addons\SharedMedia_HIPS\sound\ZugZugKick.mp3]])
 
 --statusbar:
 
